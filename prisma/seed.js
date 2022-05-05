8c66ffe643ff52bce7b53fe98135cad89ec5489a
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-05-05 12:23:18 -0500

    Agregando 2 explorers a la tabla

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,56 +3,26 @@ const prisma = new PrismaClient();
 
 (async function main() {
   try {
-    const woopa = await prisma.explorer.upsert({
-      where: { name: 'Woopa' },
+    const explorer1 = await prisma.ExplorerInfo.upsert({
+      where: { name: 'Armando Ruiz' },
       update: {},
       create: {
-        name: 'Woopa',
-				username: 'ajolonauta',
-				mission: 'Node'
+        name: 'Armando Ruiz',
+				lang: 'Español',
+				missionCommander: 'Carlo'
       },
     });
 
-    const woopa1 = await prisma.explorer.upsert({
-      where: { name: 'Woopa1' },
+    const explorer2 = await prisma.ExplorerInfo.upsert({
+      where: { name: 'Jesus Anaya' },
       update: {},
       create: {
-        name: 'Woopa1',
-				username: 'ajolonauta1',
-				mission: 'Node'
+        name: 'Jesus Anaya',
+				lang: 'Ingles',
+				missionCommander: 'Carlo'
       },
     });
-
-    const woopa2 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 2' },
-      update: {},
-      create: {
-        name: 'Woopa 2',
-				username: 'ajolonauta2',
-				mission: 'Java'
-      },
-    });
-
-    const woopa3 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 3' },
-      update: {},
-      create: {
-        name: 'Woopa 3',
-				username: 'ajolonauta3',
-				mission: 'Node'
-      },
-    });
-    const woopa4 = await prisma.explorer.upsert({
-        where: { name: 'Woopa 4' },
-        update: {},
-        create: {
-          name: 'Woopa 4',
-                  username: 'ajolonauta4',
-                  mission: 'Node'
-        },
-      });
-
-    console.log('Create 4 explorers');
+    console.log('Create 2 explorers');
   } catch(e) {
     console.error(e);
     process.exit(1);

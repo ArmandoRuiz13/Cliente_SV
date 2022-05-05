661014f5b0743abf4b0ff1e1447ce8816b8e0df8
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-05-05 12:32:38 -0500

    Modificando endpoints para funcionar con la nueva tabla

--- server.js
+++ server.js
@@ -12,27 +12,27 @@ app.get('/', (req, res) => {
 });
 
 app.get('/explorers', async (req, res) => {
-    const allExplorers =  await prisma.explorer.findMany({});
+    const allExplorers =  await prisma.explorerInfo.findMany({});
     res.json(allExplorers);
   });
 
 app.get('/explorers/:id', async (req, res) => {
   const id = req.params.id;
-  const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
+  const explorer = await prisma.explorerInfo.findUnique({where: {id: parseInt(id)}});
   res.json(explorer);
 });
 
 app.post('/explorers', async (req, res) => {
     const explorer = {
       name: req.body.name,
-      username: req.body.username,
-      mission: req.body.mission
+      lang: req.body.lang,
+      missionCommander: req.body.missionCommander
      };
 
 app.put('/explorers/:id', async (req, res) => {
     const id = parseInt(req.params.id);
 
-    await prisma.explorer.update({
+    await prisma.explorerInfo.update({
         where: {
             id: id
         },
@@ -46,12 +46,12 @@ app.put('/explorers/:id', async (req, res) => {
 
 app.delete('/explorers/:id', async (req, res) => {
     const id = parseInt(req.params.id);
-    await prisma.explorer.delete({where: {id: id}});
+    await prisma.explorerInfo.delete({where: {id: id}});
     return res.json({message: "Eliminado correctamente"});
 });
 
     const message = 'Explorer creado.';
-    await prisma.explorer.create({data: explorer});
+    await prisma.explorerInfo.create({data: explorer});
     return res.json({message});
   });
 
